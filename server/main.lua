d425a47d1ebbfbff1ae52dfc1378ae39ccee6c18
hez15 <henrybannon@hotmail.co.uk>
2026-03-19 12:44:24 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -2,3 +2,29 @@ CreateThread(function()
     Wait(1000)
     print('[hbs-fuel] starter resource loaded')
 end)
+local activeNozzles = {}
+
+CreateThread(function()
+    Wait(1000)
+    print('[hbs-fuel] starter resource loaded')
+end)
+
+RegisterNetEvent('hbs-fuel:server:syncNozzleGrab', function()
+    local src = source
+    activeNozzles[src] = true
+    TriggerClientEvent('hbs-fuel:client:syncNozzleGrab', -1, src)
+end)
+
+RegisterNetEvent('hbs-fuel:server:syncNozzleReturn', function()
+    local src = source
+    activeNozzles[src] = nil
+    TriggerClientEvent('hbs-fuel:client:syncNozzleReturn', -1, src)
+end)
+
+AddEventHandler('playerDropped', function()
+    local src = source
+    if activeNozzles[src] then
+        activeNozzles[src] = nil
+        TriggerClientEvent('hbs-fuel:client:syncNozzleReturn', -1, src)
+    end
+end)
